ea84e25e028854a828e3d378e949957a1d34d168
09lkei <09lkei@brightoncollege.net>
2023-09-03 13:28:09 +0100

    restructured events

--- bot/commands/misc/command1.js
+++ bot/commands/misc/command1.js
@@ -2,7 +2,7 @@ const {SlashCommandBuilder} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("bye")
+        .setName("test")
         .setDescription("cool"),
     async execute(interaction){
         await interaction.reply("you smell like you farded!!");
--- bot/events/clientReady.js
+++ bot/events/clientReady.js
@@ -1,4 +1,9 @@
 const {Events} = require("discord.js");
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+          console.log(`Ready! Logged in as ${client.user.tag}`);
+    },
+};
