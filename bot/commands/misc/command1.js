a9d0e65f608a6b46198b5d666445d86e330a8f5d
09lkei <09lkei@brightoncollege.net>
2023-09-03 11:06:25 +0100

    First command and command handler

---
+++ bot/commands/misc/command1.js
@@ -0,0 +1,10 @@
+const {SlashCommandBuilder} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("test")
+        .setDescription("cool"),
+    async execute(interaction){
+        await interaction.reply("you smell like you farded");
+    },
+};
